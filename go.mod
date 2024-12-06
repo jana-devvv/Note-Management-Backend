180b7ccabc748d909d798b13184a0b668f65392f
jana-devvv <janadevofficial@gmail.com>
2024-12-06 12:54:02 +0800

    Added CORS
    Added CORS used gin-contrib/cors

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module jasen-dev/jd-note
 go 1.22.2
 
 require (
+	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/joho/godotenv v1.5.1
 	gorm.io/driver/postgres v1.5.11
--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"jasen-dev/jd-note/config"
 	"jasen-dev/jd-note/routes"
 	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -16,8 +18,16 @@ func main() {
 	}
 
 	config.ConnectDB()
+
 	// Setup Gin router
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH"},
+		AllowHeaders:     []string{"Content-Type", "Content-Length", "Accept-Encoding", "Authorization", "Accept", "Origin", "Cache-Control", "X-Requested-With"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	routes.SetupRoutes(r)
 
